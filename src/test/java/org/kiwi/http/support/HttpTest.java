619f9bd4bc27ed0796a9b323b1727c83dc143861
jack19910921 <870313180@qq.com>
2017-07-30 18:37:36 +0800

    修复编译问题

--- src/test/java/org/kiwi/http/support/HttpTest.java
+++ src/test/java/org/kiwi/http/support/HttpTest.java
@@ -1,13 +1,13 @@
 package org.kiwi.http.support;
 
 import com.alibaba.fastjson.JSON;
-import com.google.common.collect.Maps;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.kiwi.http.support.enums.Protocol;
 import org.kiwi.http.support.enums.RequestMethod;
 import org.kiwi.http.support.exception.HttpException;
 
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -44,14 +44,13 @@ public class HttpTest {
     @Test
     public void testDoPost() {
         try {
-
             HttpTemplate httpTemplate = new HttpTemplate.Builder()
                     .charset("UTF-8")
                     .protocol(Protocol.HTTP)
                     .build();
             String url = "http://127.0.0.1:8080/job/list";
 
-            Map<String, String> response = httpTemplate.execute(url, Maps.<String, String>newHashMap(), RequestMethod.POST,
+            Map<String, String> response = httpTemplate.execute(url, new HashMap<String, String>(), RequestMethod.POST,
 
                     new HttpCallback<Map<String, String>>() {
 
